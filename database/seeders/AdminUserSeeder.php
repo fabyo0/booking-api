2f80ed85fcb00a46bc32d23a4953e670059fb399
Emre Dikmen <emredikmen002@gmail.com>
2023-12-28 23:20:18 +0300

    admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'superadmin@booking.com',
+            'password' => Hash::make('123'),
+            'email_verified_at' => now(),
+            'role_id' => 1, // Administrator
+        ]);
+    }
+}
